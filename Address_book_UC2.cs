92850fd45622f9bb3ffcc811fe64f2c0b627d7a7
harshsingh9898 <73526355+harshsingh9898@users.noreply.github.com>
2022-06-18 04:54:41 +0530

    Add files via upload

---
+++ Address_book_UC2.cs
@@ -0,0 +1,24 @@
+﻿namespace AddressBook
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom to Address Program");
+
+            string selection = "";
+            AddressPrompt prompt = new AddressPrompt();
+
+            prompt.displayMenu();
+
+            while (!selection.ToUpper().Equals("Q"))
+            {
+                Console.WriteLine("Selection: ");
+                selection = Console.ReadLine();
+                prompt.PerformAction(selection);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
